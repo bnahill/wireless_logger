8de92dc7469156063626e1e4a1cd117bbfd1f39c
Ben Nahill <bnahill@gmail.com>
2013-01-15 18:35:43 -0500

    Fixed toolchain config: Now works with CMake 2.8.10 which would still prefer native toolchain
    Change-Id: Iac92820f500a6a57f0367c913615c4f1a3153876

--- sw_imu/toolchain.cmake
+++ sw_imu/toolchain.cmake
@@ -1,6 +1,11 @@
 # Very simple toolchain configuration where the GNU compilers are in your PATH
 # and all have a common prefix. Make sure to set TC_PREFIX elsewhere
 
+SET(CMAKE_C_COMPILER arm-none-eabi-gcc)
+SET(CMAKE_CXX_COMPILER arm-none-eabi-g++)
+SET(CMAKE_OBJDUMP arm-none-eabi-objdump)
+SET(CMAKE_OBJCOPY arm-none-eabi-objcopy)
+
 SET(CMAKE_SYSTEM_NAME  Generic)
     
 SET(CMAKE_CROSSCOMPILING 1)
